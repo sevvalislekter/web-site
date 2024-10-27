f30c2168348808803018eddfc912284e4dda81f0
sevval <sevvalislekter.328@gmail.com>
2024-10-27 21:20:59 +0300

    degisikler oldu

---
+++ source/repos/ConsoleApp29/ConsoleApp29/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Birinci sayıyı girin: ");
+        int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("İkinci sayıyı girin: ");
+        int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+        int toplam = sayi1 + sayi2;
+        Console.WriteLine($"Toplam: {toplam}");
+    }
+}
